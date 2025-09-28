76b37709fb7336a98207d2d4c6d7d8a8d54fedba
MariyaBespalova <bespalovamaschka@yandex.ru>
2025-09-28 13:16:45 +0300

    Delete Sprint_5-develop directory

--- Sprint_5-develop/helpers.py
+++
@@ -1,8 +0,0 @@
-import string
-import random
-
-def generate_random_email(length=10, domain="yandex.ru"):  #  генерация рандомного Email
-        letters_and_digits = string.ascii_lowercase + string.digits
-        random_str = ''.join(random.choice(letters_and_digits) for _ in range(length))
-        email = f"{random_str}@{domain}"
-        return email
