8f20f17c84748c2fb62bddabc9356891c0df8cf6
MariyaBespalova <bespalovamaschka@yandex.ru>
2025-09-28 10:34:09 +0300

    test_go_to_the_section.py

--- test_go_to_the_section.py
+++ test_go_to_the_section.py
@@ -4,18 +4,16 @@ from locators import Locators
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# переход в раздел Соусы
-def test_go_to_sauses(driver):
-    driver.get(Data.STELLAR_URL)
-    WebDriverWait(driver, 5).until(EC.element_to_be_clickable(Locators.ROLLS_HEADLINE))
-    driver.find_element(*Locators.SAUCE_HEADLINE).click() 
-    sauce_spicy = driver.find_element(*Locators.SAUCE_SPICY)
-    assert sauce_spicy.is_displayed()
-
-#  переход в раздел Начинки
-def test_go_to_toppings(driver):
-    driver.get(Data.STELLAR_URL)
-    WebDriverWait(driver, 5).until(EC.element_to_be_clickable(Locators.ROLLS_HEADLINE))
-    driver.find_element(*Locators.TOPPINGS_HEADLINE).click() 
-    toppings_meat = driver.find_element(*Locators.TOPPINGS_MEAT)
-    assert toppings_meat.is_displayed()
+test_data = [
+    ("Соусы", Locators.SAUCE_HEADLINE, Locators.SAUCE_SPICY),
+    ("Начинки", Locators.TOPPINGS_HEADLINE, Locators.TOPPINGS_MEAT),
+    ("Булки", Locators.ROLLS_HEADLINE, Locators.BURGER_INGREDIENT),
+]
+@pytest.mark.parametrize("section_name, section_locator, expected_element_locator", test_data)
+class TestSection:
+    def test_navigation(self, driver, section_name, section_locator, expected_element_locator):  
+        driver.get(Data.STELLAR_URL)
+        WebDriverWait(driver, 5).until(EC.element_to_be_clickable(section_locator))
+        driver.find_element(*section_locator).click() 
+        element = WebDriverWait(driver, 5).until(EC.visibility_of_element_located(expected_element_locator))
+        assert element.is_displayed(), f"{section_name} раздел не отображается"
