3160c5e784bcb30a97508d5ab3bf214c3e7916d0
MariyaBespalova <bespalovamaschka@yandex.ru>
2025-09-28 10:17:31 +0300

    test_registration.py

--- test_registration.py
+++ test_registration.py
@@ -1,43 +1,38 @@
-import random
-import string
 import pytest
 from data import Data
 from locators import Locators
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from time import sleep
+from helpers import generate_random_email
 
-def generate_random_email(length=10, domain="yandex.ru"):  #  генерация рандомного Email
-    letters_and_digits = string.ascii_lowercase + string.digits
-    random_str = ''.join(random.choice(letters_and_digits) for _ in range(length))
-    email = f"{random_str}@{domain}"
-    return email
-
-def test_registration_exists(driver):  # проверка успешной регистрации
-    driver.get(Data.REGISTRATION_URL)
-
-    random_email = generate_random_email()
-    sleep(3)
-    driver.find_element(*Locators.NAME).send_keys('Mariya')
-    driver.find_element(*Locators.EMAIL).send_keys(random_email)
-    driver.find_element(*Locators.PASSWORD).send_keys('73377310')
-
-    driver.find_element(*Locators.REGISTRATION_BUTTON).click()
-    sleep(3)
-
-    WebDriverWait(driver, 3).until(EC.visibility_of_element_located(Locators.CONSTRUCTOR_BUTTON))
+class TestRegistration:
     
-def test_incorrect_password(driver):    #  проверка ошибки некорректного пароля
-    driver.get(Data.REGISTRATION_URL)
+    def test_registration_exists(self, driver):  # проверка успешной регистрации
+        driver.get(Data.REGISTRATION_URL)
 
-    random_email = generate_random_email()
+        random_email = generate_random_email()
+        WebDriverWait(driver, 5).until(EC.element_to_be_clickable(Locators.NAME))
+        driver.find_element(*Locators.EMAIL).send_keys(random_email)
+        driver.find_element(*Locators.PASSWORD).send_keys(Data.VALID_PASSWORD)
 
-    driver.find_element(*Locators.NAME).send_keys('Mariya')
-    driver.find_element(*Locators.EMAIL).send_keys(random_email)
-    driver.find_element(*Locators.PASSWORD).send_keys('123')
-    driver.find_element(*Locators.REGISTRATION_BUTTON).click()
-    sleep(3)
+        driver.find_element(*Locators.REGISTRATION_BUTTON).click()
+        WebDriverWait(driver, 3).until(EC.visibility_of_element_located(Locators.CONSTRUCTOR_BUTTON))
+        assert Data.LOGIN_URL in driver.current_url
+    
+    def test_incorrect_password(self, driver):    #  проверка ошибки некорректного пароля
+        driver.get(Data.REGISTRATION_URL)
+
+        random_email = generate_random_email()
+
+        driver.find_element(*Locators.NAME).send_keys(Data.VALID_NAME)
+        driver.find_element(*Locators.EMAIL).send_keys(random_email)
+        driver.find_element(*Locators.PASSWORD).send_keys(Data.INVALID_PASSWORD)
+        driver.find_element(*Locators.REGISTRATION_BUTTON).click()
+        error_element = WebDriverWait(driver, 3).until(EC.visibility_of_element_located(Locators.TEXT_ERROR_PASSWORD))
+        error_text = error_element.text
+        assert 'Некорректный пароль' in error_text
 
     error_element = WebDriverWait(driver, 3).until(EC.visibility_of_element_located(Locators.TEXT_ERROR_PASSWORD))
     error_text = error_element.text
+
     assert 'Некорректный пароль' in error_text
